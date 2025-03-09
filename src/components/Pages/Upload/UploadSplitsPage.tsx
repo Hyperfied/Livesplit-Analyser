bc97af5e360e4c1cf280dcf3954cadf0b9a0f494
Hyperfied <hyperfied@outlook.com>
2025-03-09 16:10:27 +0000

    Implement Tailwind into Upload Page

--- src/components/Pages/Upload/UploadSplitsPage.tsx
+++ src/components/Pages/Upload/UploadSplitsPage.tsx
@@ -1,4 +1,3 @@
-import "./UploadSplitsPage.css";
 import React, { useState } from "react";
 import Splits from "../../../classes/Splits";
 
@@ -46,10 +45,12 @@ function UploadSplitsPage({
   };
 
   return (
-    <div className={`upload-splits-page`}>
-      <div className={`upload-splits`}>
+    <div className={`flex h-full w-full justify-center items-center`}>
+      <div
+        className={`flex flex-col items-center justify-center h-1/2 w-3/10 border-2 border-black rounded-lg p-20 text-center bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 gap-10`}
+      >
         <h1>Select your Splits</h1>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5">
           <input type="file" onChange={handleFileChange} />
           <button type="submit">Analyse</button>
         </form>
