e48074aca1d46c25a6a3611ece425662d208e84e
Hyperfied <hyperfied@outlook.com>
2025-03-09 17:40:30 +0000

    Tailwind Graphs

--- src/components/Graphs/PlaytimeGraph.tsx
+++ src/components/Graphs/PlaytimeGraph.tsx
@@ -17,8 +17,6 @@ import {
   ValueType,
 } from "recharts/types/component/DefaultTooltipContent";
 
-import "./SplitGraphs.css";
-
 interface PlaytimeGraphProps {
   splits: Splits;
   useGameTime: boolean;
@@ -68,8 +66,8 @@ const TimeSpanTooltip = ({
 }: TooltipProps<ValueType, NameType>) => {
   if (active && payload && payload.length) {
     return (
-      <div className="timespan-tooltip">
-        <p className="label">{label}</p>
+      <div className="px-10 py-2 bg-[#f5f5f5] text-center">
+        <p className="label font-bold text-xl">{label}</p>
         <p className="value">
           {payload[0] && typeof payload[0].value === "number"
             ? new TimeSpan(payload[0].value).toString(true, false)
