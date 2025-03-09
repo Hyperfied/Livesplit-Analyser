2a45856b4d80f5984ef42ccfb5d90e1ce3a47426
Hyperfied <hyperfied@outlook.com>
2025-03-09 17:12:25 +0000

    Tailwind Stat

--- src/components/Stat.tsx
+++ src/components/Stat.tsx
@@ -1,8 +1,8 @@
 function Stat({ title, value }: { title: string; value: string }) {
   return (
-    <div className="split-stats-stat">
-      <h2>{title}</h2>
-      <h3>{value}</h3>
+    <div className="flex items-center justify-between w-4/5">
+      <h2 className="text-2xl font-bold">{title}</h2>
+      <h3 className="text-xl">{value}</h3>
     </div>
   );
 }
