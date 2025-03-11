cf5fbf951f2917cd85593a79a84804d6569d55d5
Hyperfied <hyperfied@outlook.com>
2025-03-11 18:56:21 +0000

    Fit submit button

--- src/components/Pages/Upload/UploadSplitsPage.tsx
+++ src/components/Pages/Upload/UploadSplitsPage.tsx
@@ -48,11 +48,14 @@ function UploadSplitsPage({
     <div className="flex h-full w-full justify-center items-center">
       <div
         className={
-          "flex flex-col items-center justify-center h-1/3 w-3/10 border-2 border-black rounded-lg p-20 text-center bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 gap-10"
+          "flex flex-col items-center justify-center h-1/3 w-1/2 border-2 border-black rounded-lg p-20 text-center bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 gap-10"
         }
       >
         <h1 className="text-4xl font-bold">Select your Splits</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-5 w-full justify-center items-center"
+        >
           <input
             type="file"
             onChange={handleFileChange}
@@ -60,7 +63,7 @@ function UploadSplitsPage({
           />
           <button
             type="submit"
-            className="text-lg bg-stone-200 border-0 rounded-full px-2 py-1 font-semibold cursor-pointer hover:bg-stone-300"
+            className="text-lg bg-stone-200 border-0 rounded-full w-fit px-2 py-1 font-semibold cursor-pointer hover:bg-stone-300"
           >
             Analyse
           </button>
