00edcce1d70bdbd2b14f5746118ec2d8d79cbb29
Hyperfied <hyperfied@outlook.com>
2025-03-17 11:36:48 +0000

    Use justify between for segment titles

--- src/components/SubsplitItem.tsx
+++ src/components/SubsplitItem.tsx
@@ -8,7 +8,7 @@ interface SubsplitItemProps {
 
 function SubsplitItem({name, time, setCurrentSegment}: SubsplitItemProps) {
     return (
-        <div className="flex flex-row justify-around items-center border-b-2 h-30 cursor-pointer p-4">
+        <div className="flex flex-row justify-between items-center border-b-2 h-30 cursor-pointer p-4">
             <h2 className="text-3xl font-bold">{name}</h2>
             <h3 className="text-3xl">{time}</h3>
         </div>
