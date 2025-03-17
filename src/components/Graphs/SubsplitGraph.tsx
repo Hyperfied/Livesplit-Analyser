096c387f2af125bab9d6a671c3a5a5eb4772988e
Hyperfied <hyperfied@outlook.com>
2025-03-17 18:42:38 +0000

    Use proper domain for segment sob

--- src/components/Graphs/SubsplitGraph.tsx
+++ src/components/Graphs/SubsplitGraph.tsx
@@ -13,26 +13,32 @@ import TimeSpanTooltip from "./TimeSpanTooltip";
 import Segment from "../../classes/Segment";
 
 interface SubsplitGraphProps {
-  useGameTime: boolean,
-  segment: Segment
+  useGameTime: boolean;
+  segment: Segment;
 }
 
 function SubsplitGraph({ useGameTime, segment }: SubsplitGraphProps) {
   return (
-    <ResponsiveContainer width="90%" height="100%">
-      <LineChart data={segment.getSegmentTimeGraphData(useGameTime)}>
-        <Line
-          connectNulls
-          type="monotone"
-          dataKey="Time"
-          stroke="#8884d8"
-          dot={{ r: 1 }}
-        />
+    <ResponsiveContainer width="95%" height="90%">
+      <LineChart
+        data={segment.getSegmentTimeGraphData(useGameTime)}
+        {...{ overflow: "visible" }}
+      >
+        <Line type="monotone" dataKey="Time" stroke="#8884d8" dot={{ r: 1 }} />
         <CartesianGrid stroke="#ccc" />
         <XAxis dataKey="RunId">
           <Label value="RunId" offset={-20} position="insideBottom" />
         </XAxis>
-        <YAxis tick={<TimeSpanTick />} /> {/* TODO: Add domain */}
+        <YAxis
+          tick={<TimeSpanTick />}
+          domain={[
+            useGameTime
+              ? segment.bestSegmentGameTime.totalMilliseconds
+              : segment.bestSegmentRealTime.totalMilliseconds,
+            "auto",
+          ]}
+        />
+
         <Tooltip content={<TimeSpanTooltip />} />
       </LineChart>
     </ResponsiveContainer>
