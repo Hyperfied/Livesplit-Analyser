ccfe0421517251f84dba97547b9a45d3ca0e85b5
Hyperfied <hyperfied@outlook.com>
2025-03-17 12:31:39 +0000

    Add changing segment

--- src/components/SubsplitItem.tsx
+++ src/components/SubsplitItem.tsx
@@ -3,12 +3,17 @@ import Segment from "../classes/Segment"
 interface SubsplitItemProps {
     name: string,
     time: string,
-    setCurrentSegment: React.Dispatch<React.SetStateAction<Segment | null | undefined>>
+    setCurrentSegment: React.Dispatch<React.SetStateAction<Segment | null | undefined>>,
+    segment: Segment
 }
 
-function SubsplitItem({name, time, setCurrentSegment}: SubsplitItemProps) {
+function SubsplitItem({name, time, setCurrentSegment, segment}: SubsplitItemProps) {
+
+    function onClick() {
+        setCurrentSegment(segment);
+    }
     return (
-        <div className="flex flex-row justify-between items-center border-b-2 h-30 cursor-pointer p-4">
+        <div className="flex flex-row justify-between items-center border-b-2 h-30 cursor-pointer p-4" onClick={onClick}>
             <h2 className="text-3xl font-bold">{name}</h2>
             <h3 className="text-3xl">{time}</h3>
         </div>
