86ab05b29978a93615a2cbbf5f151a77c9c6a104
Hyperfied <hyperfied@outlook.com>
2025-03-17 18:45:43 +0000

    Add on hover to subsplititem

--- src/components/SubsplitItem.tsx
+++ src/components/SubsplitItem.tsx
@@ -1,23 +1,32 @@
-import Segment from "../classes/Segment"
+import Segment from "../classes/Segment";
 
 interface SubsplitItemProps {
-    name: string,
-    time: string,
-    setCurrentSegment: React.Dispatch<React.SetStateAction<Segment | null | undefined>>,
-    segment: Segment
+  name: string;
+  time: string;
+  setCurrentSegment: React.Dispatch<
+    React.SetStateAction<Segment | null | undefined>
+  >;
+  segment: Segment;
 }
 
-function SubsplitItem({name, time, setCurrentSegment, segment}: SubsplitItemProps) {
-
-    function onClick() {
-        setCurrentSegment(segment);
-    }
-    return (
-        <div className="flex flex-row justify-between items-center border-b-2 h-30 cursor-pointer p-4" onClick={onClick}>
-            <h2 className="text-3xl font-bold">{name}</h2>
-            <h3 className="text-3xl">{time}</h3>
-        </div>
-    )
+function SubsplitItem({
+  name,
+  time,
+  setCurrentSegment,
+  segment,
+}: SubsplitItemProps) {
+  function onClick() {
+    setCurrentSegment(segment);
+  }
+  return (
+    <div
+      className="flex flex-row justify-between items-center border-b-2 h-30 cursor-pointer p-4 hover:bg-stone-300"
+      onClick={onClick}
+    >
+      <h2 className="text-3xl font-bold">{name}</h2>
+      <h3 className="text-3xl">{time}</h3>
+    </div>
+  );
 }
 
 export default SubsplitItem;
