b2a76a551bcc30790176d9ab751b79640f6c5fb6
Hyperfied <hyperfied@outlook.com>
2025-03-17 10:57:42 +0000

    Map over segments

--- src/components/Panels/SubsplitsPanel.tsx
+++ src/components/Panels/SubsplitsPanel.tsx
@@ -1,11 +1,19 @@
 import SubsplitItem from "../SubsplitItem";
 import SubsplitsStats from "../SubsplitsStats";
 
+import SplitsContext from "../../classes/SplitsContext";
+import { useContext } from "react";
+
 function SubsplitsPanel() {
+  const splits = useContext(SplitsContext)
+  if (!splits) {
+    return <div></div>
+  }
+
     return (
     <div className="flex h-3/4 w-9/10 border-2 rounded-lg bg-white">
         <div className="h-full w-1/3 border-r-2 overflow-auto">
-          <SubsplitItem name="First Steps" time="2:01"/>
+          {splits.segments.map(segment => <SubsplitItem name={segment.name} time={segment.bestSegmentGameTime.toString(true, false)}/>)}
         </div>
         <SubsplitsStats/>
       </div>
